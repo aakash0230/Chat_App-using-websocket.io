bed7ae54f71cf991b619866c5d779d1af2954fa6
Aakash Pandey <pandeyaakash123.ap@gmail.com>
2022-03-30 15:22:07 +0530

    connection and disconnection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketIo = require("socket.io");
 
 const port = process.env.PORT || 3000;
-
 const publicPath = path.join(__dirname,"/../public");
-
-const app = express()
+let app = express()
+let server = http.createServer(app);
+let io = socketIo(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// connnecting the client and server
+io.on("connection", (socket) => {
+    console.log("A new user just connected");
+    
+    // disconnected from the server
+    socket.on("disconnect", () => {
+        console.log("A new user just disconnected");
+    });
+});
+
+
+server.listen(port, () => {
     console.log("logging in server");
 })
